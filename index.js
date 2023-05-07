1dde4f9a8aa396ef96aca568a1a40ce9b3304191
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-05-07 16:28:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ const titleCased = (words) => {
   words = tutorials.map(tutorial => 
     tutorial.replace(/\b\w/g, match => match.toUpperCase())
   );
-  return words;
+  return words; //This is a chatgpt code OBVIOUSLY.... I tried to solve it and I got to the point where I turned the whole array to
+               //a sting and made all the letters uppercase. I got bored and tried chatgpt and it gave me a wonderful solution. I really liked 
+               //it and learned more about regex. So I will push this :)
 }
